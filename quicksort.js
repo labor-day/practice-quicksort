62b58513033b74703cb4cee8e295ae5999a24784
labor-day <shaffer.scott@yahoo.com>
2022-07-12 19:57:27 -0600

    complete

--- quicksort.js
+++ quicksort.js
@@ -1,18 +1,37 @@
 function quicksort(arr) {
 
   // Check if the input is length 1 or less
-    // If so, it's already sorted: return
+  if (arr.length <= 1) {
+     // If so, it's already sorted: return
+    return arr;
+  }
 
   // Pick the first value as the pivot
+  let pivot = arr[0];
+
+  let left = [];
+  let right = [];
 
   // Orient the pivot so that...
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
       // every number smaller than the pivot is to the left
       // every number larger (or equal) than the pivot is to the right
 
+  }
+
+
   // Recursively sort the left
+  left = quicksort(left);
   // Recursively sort the right
+  right = quicksort(right);
 
   // Return the left, pivot and right in sorted order
+  return [...left, pivot, ...right];
 
 }
 
